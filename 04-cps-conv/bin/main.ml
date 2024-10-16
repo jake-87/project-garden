e2b7decb0686d51b5bf70cf7b4193649746feecd
Wren H <>
2024-10-16 20:13:38 +1100

    opts

--- 04-cps-conv/bin/main.ml
+++ 04-cps-conv/bin/main.ml
@@ -23,6 +23,62 @@ and cval =
   | Cin of int * string
   | Cfn of string * string * ctm
 
+let rec count (tm: ctm) s =
+  let i x = if x = s then 1 else 0 in
+  match tm with
+  | Cletv (_, a, b) -> count_v a s + count b s
+  | Clet(a,_,q,_) when a = s && q = s -> 1
+  | Clet(a,_,_,_) when a = s -> 0
+  | Clet (_, _, q, e) -> i q + count e s
+  | Cletc(a,k,_,_) when a = s || k = s -> 0
+  | Cletc (_, _, a, b) -> count a s + count b s
+  | Cappc (a, b) -> i a + i b
+  | Capp (a, b, c) -> i a + i b + i c
+  | Ccase (a, b, c) -> i a + i b + i c
+and count_v v s =
+  let i x = if x = s then 1 else 0 in
+    match v with
+  | CU -> 0
+  | Cpair (a, b) -> i a + i b
+  | Cin (_, a) -> i a
+  | Cfn (a, b, _) when a = s || b = s -> 0
+  | Cfn(_,_,e) -> count e s
+
+let rec subst_v tm s1 s2 =
+  match tm with
+  | Cpair(a,b) when a = b && b = s1 -> Cpair(s2, s2)
+  | Cpair(a,b) when a = s1 -> Cpair(s2, b)
+  | Cpair(a,b) when b = s1 -> Cpair(a, s2)
+  | Cin(i,e) when e = s1 -> Cin(i, s2)
+  | Cfn(k,_,_) when k = s1 -> tm
+  | Cfn(k,a,e) when a = s1 -> Cfn(k, s2, subst e s1 s2)
+  | e -> e
+
+and subst e q w =
+  match e with
+  | Cletv(s,v,e) when s = q -> Cletv(s,subst_v v q w,e)
+  | Cletv(s,v,e) -> Cletv(s,subst_v v q w,subst e q w)
+  | Clet(s,i,p,e) when s = q && p = q -> Clet(s,i,w,e)
+  | Clet(s,_i,_p,e) when s = q -> e
+  | Clet(s,i,p,e) when p = q -> Clet(s,i,w,subst e q w)
+  | Clet(s,i,p,e) -> Clet(s,i,p,subst e q w)
+  | Cletc(s,b,r,t) when s = q -> Cletc(s,b,subst r q w, t)
+  | Cletc(s,p,e1,e2) when p = q -> Cletc(s,w,subst e1 q w,subst e2 q w)
+  | Cletc(s,p,e1,e2) -> Cletc(s,p,subst e1 q w,subst e2 q w)
+  | Cappc(s,p) when s = q && p = q -> Cappc(w,w)
+  | Cappc(s,p) when s = q -> Cappc(w,p)
+  | Cappc(s,p) when p = q -> Cappc(s,w)
+  | Cappc(s,p) -> Cappc(s,p)
+  | Capp(a,b,c) ->
+     let sel a = if a = q then w else a in
+     let a,b,c = sel a, sel b, sel c in
+     Capp(a,b,c)
+  | Ccase(a,b,c) ->
+     let sel a = if a = q then w else a in
+     let a,b,c = sel a, sel b, sel c in
+     Ccase(a,b,c)
+
+
 let rec spaces i =
   if i = 0 then ""
   else "  " ^ (spaces (i - 1))
@@ -89,18 +145,18 @@ let rec sq tm k =
         let@ z = sq a in
         let x=fv() in
         Clet(x,i,z,k x)
-    | Fn(a,e) ->
+    | Fn(x,e) ->
         let f=fk()in
         let k'=fk()in
-        let x=fv()in
+
         Cletv(k', Cfn(k',x,rd e k'), k f)
-    | Let(a,e1,e2) ->
+    | Let(x,e1,e2) ->
         let j=fj()in
-        let x=fv()in
+
         Cletc(j,x,sq e2 k,rd e1 j)
-    | Case(e,a,e1,b,e2) ->
+    | Case(e,x1,e1,x2,e2) ->
         let@ z = sq e in
-        let k1,k2,x1,x2 = fk(),fk(),fv(),fv() in 
+        let k1,k2 = fk(),fk() in 
         let j, x = fj(),fv() in
         Cletc(j,x,k x,
         Cletc(k1,x1,rd e1 j,
@@ -135,9 +191,9 @@ and rd tm k =
     | Let(x,e1,e2) ->
         let j=fj()in
         Cletc(j,x,rd e2 k,rd e1 j)
-    | Case(e,a,e1,b,e2) ->
+    | Case(e,x1,e1,x2,e2) ->
         let@ z = sq e in
-        let k1,k2,x1,x2 = fk(),fk(),fv(),fv() in
+        let k1,k2 = fk(),fk() in
         Cletc(k1,x1,rd e1 k,
         Cletc(k2,x2,rd e2 k,
         Ccase(z,k1,k2)
@@ -145,40 +201,54 @@ and rd tm k =
         )
 
 let rec hoist tm =
-    let f = hoist in
-    match tm with
-    | Cletc(a,b,e1,Cletv(q,v,e2)) ->
-        Cletv(q,v,Cletc(a,b,e1,e2))
-    | Cletc(a,b,e1,e2) -> Cletc(a,b,f e1,f e2)
-    | Clet(a,i,b,e) -> Clet(a,i,b,f e)
-    | Cletv(a,v,e) -> Cletv(a,v,f e)
-    | t -> t
-(*
-  | Cletv of string * cval * ctm
-  | Clet of string * int * string * ctm
-  | Cletc of string * string * ctm * ctm
-  | Cappc of string * string
-  | Capp of string * string * string
-  | Ccase of string * string * string
-*)
-let fixish tm =
-    let rec go i t =
-        if i = 0 then t
-        else go (i - 1) (hoist t)
-    in
-    go 100 tm
+  let f = hoist in
+  match tm with
+  (* rewrite *)
+  | Cletc(a,b,e1,Cletv(q,v,e2)) ->
+     Cletv(q,v,Cletc(a,b,e1,e2))
+  (* inline *)
+  | Cletc(a,b,Cappc(q,w),e) when b = w ->
+     subst e a q
+  (* inline *)
+  | Cletc(a,b,body,Cappc(q,w)) when q = a ->
+     print_endline "\nme?";
+     print_endline b;
+     print_endline w;
+     Cletc(a,b,body,subst body b w)
+
+  (* dead value elim *)
+  | Cletc(a,_,_,e1) when count e1 a = 0 -> e1
+  | Clet(a,_,_,e1) when count e1 a = 0 -> e1
+  | Cletv(a,_,e1) when count e1 a = 0 -> e1
+
+  (* propogate *)
+  | Cletc(a,b,e1,e2) -> Cletc(a,b,f e1,f e2)
+  | Clet(a,i,b,e) -> Clet(a,i,b,f e)
+  | Cletv(a,v,e) -> Cletv(a,hoist_v v,f e)
+  | t -> t
+
+and hoist_v (tm: cval) =
+  match tm with
+  | Cfn (a, b, e) -> Cfn(a,b,hoist e)
+  | _ -> tm
+
+let rec fixish tm =
+  print_endline "\n\n-------------------\n\n";
+  p_ctm 0 tm;
+  let n = hoist tm in
+  if n = tm then n
+  else fixish n 
 
 let () =
   let example =
-    Let("pair", Fn("x",Fn("y",Pair(V"x",V"y"))),
-        Let("swap", Fn("arg", Case(V"arg","a",In(1,V"a"),"b",In(0,V"b"))),
+        Let("oddid", Fn("arg", Case(V"arg","a",V"a","b",V"b")),
             Let("arg",In(0,V"a"),
-            App(V"swap",V"arg"))
-          )
+            App(V"oddid",App(V"oddid",V"arg")))
       )
   in
   let as_cps = rd example "NEXT" in
   p_ctm 0 as_cps;
   print_endline "\n\nhoist:\n\n";
   let h = fixish as_cps in
+  print_endline "\n\n";
   p_ctm 0 h
